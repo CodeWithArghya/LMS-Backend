ca41fa69f0528f0cbbf476b5215fc0fc3ec54698
Arghyadip <codewitharghya0@gmail.com>
2025-06-13 20:59:06 +0530

    m

--- backendpart/settings.py
+++ backendpart/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-d&!v%_xe4tpwm68q!-1yfd=!sq+9(fe9@j&x*0y!=gr5%d9y40
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -91,10 +91,10 @@ WSGI_APPLICATION = 'backendpart.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',  # Use PostgreSQL engine
-        'NAME': 'lms',              # Replace with your database name
+        'NAME': 'LMS',              # Replace with your database name
         'USER': 'postgres',              # Replace with your database user
-        'PASSWORD': 'Arghya2002',      # Replace with your database password
-        'HOST': 'lms.c52244sqy91z.ap-south-1.rds.amazonaws.com',# Use 'localhost' for local setup or the IP address of the database server
+        'PASSWORD': 'Arghya@2002',      # Replace with your database password
+        'HOST': 'localhost',                       # Use 'localhost' for local setup or the IP address of the database server
         'PORT': '5432',                            # Default PostgreSQL port
     }
 }
@@ -177,8 +177,17 @@ FILE_UPLOAD_MAX_MEMORY_SIZE = 52428800  # 50MB
 
  
 
-# for local
+# for prod
+AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
+AWS_S3_REGION_NAME = os.getenv('AWS_S3_REGION_NAME', 'ap-south-1')
+AWS_S3_SIGNATURE_NAME = 's3v4'
+AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACT = None
 
+# for local
 AWS_ACCESS_KEY_ID= 'AKIA5MSUBVORPWUDFLPJ'
 AWS_SECRET_ACCESS_KEY='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'
 AWS_STORAGE_BUCKET_NAME='lmseduhub'
@@ -193,15 +202,17 @@ AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 
 
 STORAGES = {
+
+    # Media file (image) management   
     "default": {
         "BACKEND": "storages.backends.s3boto3.S3StaticStorage",
     },
+    
+    # CSS and JS file management
     "staticfiles": {
         "BACKEND": "storages.backends.s3boto3.S3StaticStorage",
     },
 }
-
-
 
 
 
